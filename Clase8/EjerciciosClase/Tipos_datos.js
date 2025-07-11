445ec992d5e5f362913a877dea4953e1cc6c46b3
Nadia <nadiabarolin@gmail.com>
2025-07-11 18:02:56 -0300

    Eliminé archivos antiguos de clases anteriores

--- Clase8/EjerciciosClase/Tipos_datos.js
+++
@@ -1,8 +0,0 @@
-// Definimos diferentes tipos de datos
-let numero = 42;       // Tipo número
-let texto = "Hola mundo!";   // Tipo string o cadena de texto
-let booleano = true;      // Tipo boolean (verdadero/true o falso/false)
-let indefinido;         // Variable no inicializada (Undefined)
-let nulo = null         // Valot nulo (vacio)
-let objeto = { nombre: "Luis", edad: 30 }; // Objeto con diferentes propiedades
-let
--- Clase8/EjerciciosClase/Variables_constante.js
+++
@@ -1,6 +0,0 @@
-// Declarar variable de tipo let y const
-let nombre = "Nadia";
-const edad = 32;
-
-console.log("Hola, mi nombre es: " + nombre + "y tengo"+ edad + " años.");
-console.log('Hola,')
